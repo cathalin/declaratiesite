77e7f2638e9700d06fe49cb44503136d2c3e9496
declaratiesite <declaratiesite@protonmail.com>
2020-03-25 23:09:27 +0200

    Updates

---
+++ precache-manifest.b84b6a7ea70fa8220727568f6475b45a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e155703b741fbfc8aac25fdb7bc1b87",
+    "url": "/declaratiesite/index.html"
+  },
+  {
+    "revision": "365487c09d1ce162ce0e",
+    "url": "/declaratiesite/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "91be114cd1439586c48f",
+    "url": "/declaratiesite/static/js/2.a7c72dab.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/declaratiesite/static/js/2.a7c72dab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "365487c09d1ce162ce0e",
+    "url": "/declaratiesite/static/js/main.7873e11c.chunk.js"
+  },
+  {
+    "revision": "a1c18ba0be6411af199f",
+    "url": "/declaratiesite/static/js/runtime-main.63b8a5ab.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/declaratiesite/static/media/logo.5d5d9eef.svg"
+  }
+]);
